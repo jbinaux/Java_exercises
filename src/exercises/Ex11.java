a8c04d2a9a61bc21139723ce2e750fd29a2a1ef2
jbinaux <37798129+jbinaux@users.noreply.github.com>
2018-07-12 14:46:21 +0200

    improved ex11 (calcultation of pi)

--- src/exercises/Ex11.java
+++ src/exercises/Ex11.java
@@ -2,29 +2,26 @@ package exercises;
 
 public class Ex11 {
 
-	public static double calc(double a) {
+	public static double calc(int iterations) {
 		double result = 0;
-		double b = 1;
-		double c=0;
-		for(int i = 1; i <= 6; i++)
+		double diviseur = 1;
+		for(int i = 1; i <= iterations; i++)
 		{
 			if(i%2==0)
 			{
-				c = b*-1;
+				result -= 1/diviseur;
 			}
 			else
 			{
-				c = b;
+				result += 1/diviseur;
 			}
-			
-			result= result + (1.0/c);
-			b= b+2;
+			diviseur += 2;
 		}
-		return(a*result);
+		return(4.0*result);
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(calc(4.0));
+		System.out.println(calc(2000000000));
 	}
 
 }
